7053dc1a17b6fecba099a97b570465fb6f869647
Issif <issif_github@gadz.org>
2021-03-23 00:22:37 +0100

    add setup of kubeless

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-multipass launch --name k3s-master --cpus 1 --mem 2048M --disk 5G
+# Bootstrap cluster
+multipass launch --name k3s-master --cpus 1 --mem 2048M --disk 10G
 multipass launch --name k3s-node1 --cpus 1 --mem 2048M --disk 15G
 multipass launch --name k3s-node2 --cpus 1 --mem 2048M --disk 15G
 multipass exec k3s-master -- /bin/bash -c "curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 sh -"
@@ -13,9 +14,13 @@ export KUBECONFIG=~/.kube/k3s.yaml
 multipass exec k3s-master -- /bin/bash -c "sudo mkdir -p /var/lib/rancher/audit"
 multipass exec k3s-master -- /bin/bash -c "wget https://raw.githubusercontent.com/falcosecurity/evolution/master/examples/k8s_audit_config/audit-policy.yaml"
 multipass exec k3s-master -- /bin/bash -c "sudo cp audit-policy.yaml /var/lib/rancher/audit/"
-helm repo add falcosecurity https://falcosecurity.github.io/charts
-helm install falco falcosecurity/falco --set falcosidekick.enabled=true --set falcosidekick.webui.enabled=true --set falcosidekick.image.tag=latest --set falcosidekick.webui.image.tag=v1-beta --set auditLog.enabled=true -n falco --create-namespace
 sleep 2
+
+# Install falco + falcosdekick + webui
+helm repo add falcosecurity https://falcosecurity.github.io/charts
+helm install falco falcosecurity/falco --set falcosidekick.enabled=true --set falcosidekick.webui.enabled=true --set falcosidekick.image.tag=latest --set falcosidekick.webui.image.tag=v1-beta --set falcosidekick.webui.retention=200 --set auditLog.enabled=true --set falcosidekick.config.kubeless.namespace=kubeless --set falcosidekick.config.kubeless.function=delete-pod -n falco --create-namespace
+
+# Set up audit log endpoint (falco)
 export FALCO_SVC_ENDPOINT=$(kubectl get svc -n falco --field-selector metadata.name=falco -o=json | jq -r ".items[] | .spec.clusterIP")
 cat <<EOF > webhook-config.yaml
 apiVersion: v1
@@ -42,3 +47,75 @@ multipass exec k3s-master -- /bin/bash -c "sudo echo '    --kube-apiserver-arg=a
 multipass exec k3s-master -- /bin/bash -c "sudo chmod o-w /etc/systemd/system/k3s.service"
 multipass exec k3s-master -- /bin/bash -c "sudo systemctl daemon-reload"
 multipass exec k3s-master -- /bin/bash -c "sudo systemctl restart k3s"
+
+# Install kubeless
+export RELEASE=$(curl -s https://api.github.com/repos/kubeless/kubeless/releases/latest | grep tag_name | cut -d '"' -f 4)
+kubectl create ns kubeless
+kubectl create -f https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless-$RELEASE.yaml
+cat <<EOF | kubectl apply -n kubeless -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: falco-pod-delete
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: falco-pod-delete-cluster-role
+rules:
+  - apiGroups: [""]
+    resources: ["pods"]
+    verbs: ["get", "list", "delete"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: falco-pod-delete-cluster-role-binding
+roleRef:
+  kind: ClusterRole
+  name: falco-pod-delete-cluster-role
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+  - kind: ServiceAccount
+    name: falco-pod-delete
+    namespace: kubeless
+EOF
+cat <<EOF | kubectl apply -n kubeless -f -
+apiVersion: kubeless.io/v1beta1
+kind: Function
+metadata:
+  finalizers:
+    - kubeless.io/function
+  generation: 1
+  labels:
+    created-by: kubeless
+    function: delete-pod
+  name: delete-pod
+spec:
+  checksum: sha256:a68bf570ea30e578e392eab18ca70dbece27bce850a8dbef2586eff55c5c7aa0
+  deps: |
+    kubernetes>=12.0.1
+  function-content-type: text
+  function: |-
+    from kubernetes import client,config
+
+    config.load_incluster_config()
+
+    def delete_pod(event, context):
+        rule = event['data']['rule'] or None
+        output_fields = event['data']['output_fields'] or None
+
+        if rule and rule == "Terminal shell in container" and output_fields:
+            if output_fields['k8s.ns.name'] and output_fields['k8s.pod.name']:
+                pod = output_fields['k8s.pod.name']
+                namespace = output_fields['k8s.ns.name']
+                print (f"Deleting pod \"{pod}\" in namespace \"{namespace}\"")
+                client.CoreV1Api().delete_namespaced_pod(name=pod, namespace=namespace, body=client.V1DeleteOptions())
+  handler: delete-pod.delete_pod
+  runtime: python3.7
+  deployment:
+    spec:
+      template:
+        spec:
+          serviceAccountName: falco-pod-delete
+EOF
